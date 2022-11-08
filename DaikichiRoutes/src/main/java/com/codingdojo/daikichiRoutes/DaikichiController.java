40420e6aa47393051b4f0c7baea0653d9d4565b3
Ryan Beitler <rybeitler@gmail.com>
2022-11-08 18:56:29 -0500

    added daikichi pt2

--- DaikichiRoutes/src/main/java/com/codingdojo/daikichiRoutes/DaikichiController.java
+++ DaikichiRoutes/src/main/java/com/codingdojo/daikichiRoutes/DaikichiController.java
@@ -1,7 +1,9 @@
 package com.codingdojo.daikichiRoutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/daikichi")
@@ -17,5 +19,17 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "this is tommmorows fortune";
+	}
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city")String city) {
+		return "Congrats, you will soon travel to "+city+"!";
+	}
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num")int num) {
+		if(num % 2 ==0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		}else {
+			return "You have enjoyed the fruits of your labor, but now is a great time to spend time with family and friends";
+		}
 	}
 }
