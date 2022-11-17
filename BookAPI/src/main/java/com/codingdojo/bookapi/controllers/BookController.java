ce2d8ba48b4fa5d8f2b020c153fd56bd562aa375
Ryan Beitler <rybeitler@gmail.com>
2022-11-17 18:07:51 -0500

    2nd part of bookapi

---
+++ BookAPI/src/main/java/com/codingdojo/bookapi/controllers/BookController.java
@@ -0,0 +1,23 @@
+package com.codingdojo.bookapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.codingdojo.bookapi.models.Book;
+import com.codingdojo.bookapi.services.BookService;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+	@GetMapping("/books/{id}")
+	public String showBook(@PathVariable("id") Long id, Model model) {
+		Book book = bookService.findBook(id);
+		model.addAttribute("book", book);
+		return "show.jsp";
+	}
+}
